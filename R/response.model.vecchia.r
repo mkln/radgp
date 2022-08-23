2798aa16965edcfc190e6c03f7932bf517fa238a
mkln <peruzzi@gmail.com>
2022-08-23 09:02:27 +0200

    debug yichen's error message

--- R/response.model.vecchia.r
+++ R/response.model.vecchia.r
@@ -17,7 +17,7 @@ response.model.vecchia <- function(y, coords, m, mcmc, n_threads,
   coords_mm <- coords[ixmm,]
   y_mm <- y[ixmm]
   nn_dag_mat <- GPvecchia:::findOrderedNN_kdtree2(coords_mm, m)
-  nn_dag <- nn_dag_mat %>% apply(1, \(x) x[!is.na(x)][-1]-1)
+  nn_dag <- apply(nn_dag_mat, 1, function(x){ x[!is.na(x)][-1]-1 })
   
   maxmin_time <- system.time({    
     response_model <- altdaggp_custom(y_mm, coords_mm, nn_dag, mcmc, n_threads,
